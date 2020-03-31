804c8daccf55ac519b0d30e98d77e6b04ac43c66
moqi <moqimoqidea@gmail.com>
2020-03-31 22:53:28 +0800

    Update

---
+++ leetcode/q0752-open-the-lock/solution.py
@@ -0,0 +1,11 @@
+"""
+https://leetcode-cn.com/problems/open-the-lock/
+https://leetcode-cn.com/problems/open-the-lock/solution/da-kai-zhuan-pan-suo-by-leetcode/
+https://leetcode-cn.com/problems/open-the-lock/solution/python-bfs-qing-xi-ti-jie-by-knifezhu/
+"""
+from typing import List
+
+
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+        pass
