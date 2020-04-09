6dd6dcc87b1c91f2c7d9141ebb6ba160fd02e32f
moqi <moqimoqidea@gmail.com>
2020-04-09 22:02:26 +0800

    20200409

---
+++ leetcode/q0225-implement-stack-using-queues/solution1.py
@@ -0,0 +1,28 @@
+"""
+https://leetcode-cn.com/problems/implement-stack-using-queues/
+https://leetcode-cn.com/problems/implement-stack-using-queues/solution/yong-dui-lie-shi-xian-zhan-by-leetcode/
+https://leetcode-cn.com/problems/implement-stack-using-queues/solution/python3-list-dequeshi-xian-by-xilepeng/
+"""
+
+
+class MyStack:
+
+    def __init__(self):
+        self.data = []
+
+    def push(self, x: int) -> None:
+        self.data.append(x)
+
+        for i in range(len(self.data), 1, -1):
+            # 反转前面 n-1 个元素，栈顶元素始终保留在队首
+            self.data.append(self.data.pop(0))
+
+
+    def pop(self) -> int:
+        return self.data.pop(0)
+
+    def top(self) -> int:
+        return self.data[0]
+
+    def empty(self) -> bool:
+        return not bool(self.data)
