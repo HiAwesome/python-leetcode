94e7937c7f965f2429370b6adbd1803ca2ad2eb5
moqi <moqimoqidea@gmail.com>
2020-04-09 21:33:58 +0800

    20200409

--- leetcode/q232-implement-queue-using-stacks/solution.py
+++ leetcode/q232-implement-queue-using-stacks/solution.py
@@ -1,22 +1,32 @@
 """
 https://leetcode-cn.com/problems/implement-queue-using-stacks/
 https://leetcode-cn.com/problems/implement-queue-using-stacks/solution/yong-zhan-shi-xian-dui-lie-by-leetcode/
+https://leetcode-cn.com/problems/implement-queue-using-stacks/solution/bzhan-tu-jie-leetcode-232-yong-zhan-mo-ni-dui-lie-/
 """
 
 
 class MyQueue:
 
     def __init__(self):
-        pass
+        self.a = []
+        self.b = []
 
     def push(self, x: int) -> None:
-        pass
+        self.a.append(x)
 
     def pop(self) -> int:
-        pass
+        if not self.b:
+            while self.a:
+                self.b.append(self.a.pop())
+
+        return self.b.pop()
 
     def peek(self) -> int:
-        pass
+        if not self.b:
+            while self.a:
+                self.b.append(self.a.pop())
+
+        return self.b[-1]
 
     def empty(self) -> int:
-        pass
+        return not self.a and not self.b
