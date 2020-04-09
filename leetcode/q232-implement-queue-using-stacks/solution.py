742402b92dcf76f4da4773b5d74af2f8f26cc09e
moqi <moqimoqidea@gmail.com>
2020-04-09 21:23:41 +0800

    20200409

---
+++ leetcode/q232-implement-queue-using-stacks/solution.py
@@ -0,0 +1,22 @@
+"""
+https://leetcode-cn.com/problems/implement-queue-using-stacks/
+https://leetcode-cn.com/problems/implement-queue-using-stacks/solution/yong-zhan-shi-xian-dui-lie-by-leetcode/
+"""
+
+
+class MyQueue:
+
+    def __init__(self):
+        pass
+
+    def push(self, x: int) -> None:
+        pass
+
+    def pop(self) -> int:
+        pass
+
+    def peek(self) -> int:
+        pass
+
+    def empty(self) -> int:
+        pass
